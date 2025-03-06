b55e00bbb2f14bc4e561d3c6ff678c2bd6a00591
Prince-213 <57283635+Prince-213@users.noreply.github.com>
2025-03-06 15:46:37 +0100

    layout seo added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,6 +26,18 @@
 	});
 </script>
 
+<svelte:head>
+  <!-- Favicon -->
+  <link rel="icon" type="image/png" href="/favicon.png">
+
+  <!-- Open Graph (OG) Meta Tags for Social Sharing -->
+  <meta property="og:title" content="Precision Chronicles">
+  <meta property="og:description" content="Welcome to Precision Chronicles, a distinguished hub for academic excellence and scholarly discourse. As a premier organization dedicated to the dissemination of cutting-edge research.">
+  <meta property="og:image" content="/frame2.png"> <!-- Change to your logo's URL -->
+  <meta property="og:url" content="https://www.precisionchronicles.org/">
+  <meta name="twitter:card" content="summary_large_image">
+</svelte:head>
+
 <div class=" relative font-inter min-h-screen flex flex-col justify-between ">
 	{#if !path.includes('/admin')}
 		<Header />
